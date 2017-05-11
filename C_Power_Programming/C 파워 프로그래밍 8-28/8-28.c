52d0483e78f0d893e5f65340343711b4c3fc77a5
piecemakerz <piecemakerz@naver.com>
2017-05-11 17:33:35 +0900

    8-29 : 통과한 문제

--- C_Power_Programming/C 파워 프로그래밍 8-28/8-28.c
+++ C_Power_Programming/C 파워 프로그래밍 8-28/8-28.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define STR_LEN 50
 #define BOOK_INFO_NUM 3
 
@@ -8,19 +11,23 @@ typedef struct __bookInfo {
 	int bookPrice;
 } bookInfo;
 
+void BookArrangeSave(bookInfo * books, bookInfo temp);
+
 int main(void) {
-	bookInfo books[3];
+	bookInfo books[3] = { 0, };
 
 	puts("******** 도서 정보 입력 ********");
 	for (int i = 0; i < BOOK_INFO_NUM; i++) {
+		bookInfo temp;
 		printf("%d번째 도서 정보 입력.\n", i + 1);
 		fputs("도서 제목 : ", stdout);
-		gets(books[i].bookTitle);
+		gets(temp.bookTitle);
 		fputs("출판사 명 : ", stdout);
-		gets(books[i].bookPub);
+		gets(temp.bookPub);
 		fputs("도서 가격 : ", stdout);
-		scanf("%d", &(books[i].bookPrice));
+		scanf("%d", &(temp.bookPrice));
 		getchar();
+		BookArrangeSave(books, temp);
 		printf("%d번째 입력 완료......", i + 1);
 		puts("\n");
 	}
@@ -35,4 +42,22 @@ int main(void) {
 	}
 
 	return 0;
+}
+
+void BookArrangeSave(bookInfo * books, bookInfo temp) {
+	printf("도서 제목 : %s\n", temp.bookTitle);
+	printf("출판사 명 : %s\n", temp.bookPub);
+	printf("도서 가격 : %d\n", temp.bookPrice);
+	for (int i = 0; i < BOOK_INFO_NUM; i++) {
+		printf("도서 제목 : %s\n", books[i].bookTitle);
+		if (books[i].bookTitle == 0) {
+			books[i] = temp;
+			return;
+		}
+		if (strcmp(books[i].bookTitle, temp.bookTitle) > 0) {
+			books[i + 1] = books[i];
+			books[i] = temp;
+			return;
+		}
+	}
 }
---
+++ C_Power_Programming/C 파워 프로그래밍 8-29/8-29.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STR_LEN 50
+#define BOOK_INFO_NUM 3
+
+typedef struct __bookInfo {
+	char bookTitle[STR_LEN];
+	char bookPub[STR_LEN];
+	int bookPrice;
+} bookInfo;
+
+int numOfData = 0;
+bookInfo bookInfoList[BOOK_INFO_NUM];
+
+int InsertList(bookInfo* bookPtr);
+
+int main(void) {
+	int i, num;
+	bookInfo book;
+
+	puts("******** 도서 정보 입력 ********");
+	for (int i = 0; i < BOOK_INFO_NUM; i++) {
+		printf("%d번째 도서 정보 입력.\n", i + 1);
+		fputs("도서 제목 : ", stdout);
+		gets(book.bookTitle);
+		fputs("출판사 명 : ", stdout);
+		gets(book.bookPub);
+		fputs("도서 가격 : ", stdout);
+		scanf("%d", &(book.bookPrice));
+		getchar();
+
+		num = InsertList(&book);
+		if (num == -1)
+			printf("입력에 실패하였습니다. \n");
+		else
+			printf("%d번째 입력 완료........\n\n", num);
+	}
+
+	puts("******** 도서 정보 출력 ********");
+	for (int i = 0; i < BOOK_INFO_NUM; i++) {
+		printf("%d번째 도서 정보 출력.\n", i + 1);
+		printf("도서 제목 : %s\n", bookInfoList[i].bookTitle);
+		printf("출판사 명 : %s\n", bookInfoList[i].bookPub);
+		printf("도서 가격 : %d\n", bookInfoList[i].bookPrice);
+		puts("\n");
+	}
+}
+
+int InsertList(bookInfo* bookPtr) {
+	int idx;
+	int inputIdx = numOfData;
+
+	if (numOfData >= BOOK_INFO_NUM)
+		return -1;
+
+	for (idx = 0; idx < numOfData; idx++) {
+		if (strcmp(bookInfoList[idx].bookTitle, bookPtr->bookTitle) > 0)
+		{
+			inputIdx = idx;
+			break;
+		}
+	}
+
+	for (idx = numOfData; idx > inputIdx; idx--)
+		bookInfoList[idx] = bookInfoList[idx - 1];
+
+	bookInfoList[inputIdx] = (*bookPtr);
+
+	return ++numOfData;
+}
