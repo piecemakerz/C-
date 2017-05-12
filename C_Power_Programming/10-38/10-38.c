7171effdc0ae39b554279542cbd83a9bbabd6a14
piecemakerz <piecemakerz@naver.com>
2017-05-12 17:52:49 +0900

    10-38 : 통과한 문제

---
+++ C_Power_Programming/10-38/10-38.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STR_LEN 100
+
+void PrintString(int choice, FILE * fp);
+void PrintAllString(FILE * fp);
+
+int main(void) {
+	char input[STR_LEN];
+	int choice;
+
+	FILE * fp = fopen("string.txt", "wt");
+	if (fp == NULL) {
+		puts("파일 'string.txt'가 열리지 않았습니다.");
+		return -1;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d번째 문자열 입력 : ", i + 1);
+		fgets(input, STR_LEN, stdin);
+		fputs(input, fp);
+	}
+
+	puts("입력이 완료되었습니다.");
+	fclose(fp);
+
+	fp = fopen("string.txt", "rt");
+	if (fp == NULL) {
+		puts("파일 'string.txt'가 열리지 않았습니다.");
+		return -1;
+	}
+
+	fputs("출력할 문자열 정보 입력(6이상은 전체 출력): ", stdout);
+	scanf("%d", &choice);
+	
+	if (0 < choice && choice < 6)
+		PrintString(choice, fp);
+	else if (choice >= 6)
+		PrintAllString(fp);
+	else
+		puts("적절치 않은 입력!");
+
+	puts("출력이 완료되었습니다.");
+	fclose(fp);
+	return 0;
+}
+
+void PrintString(int choice, FILE * fp) {
+	int i;
+	char str[STR_LEN];
+
+	for (i = 0; i < choice - 1; i++)
+		fgets(str, STR_LEN - 1, fp);
+
+	fgets(str, STR_LEN - 1, fp);
+	fputs(str, stdout);
+}
+
+void PrintAllString(FILE * fp) {
+	char str[STR_LEN];
+
+	while (1) {
+		fgets(str, STR_LEN - 1, fp);
+		if (feof(fp) != 0)
+			break;
+		fputs(str, stdout);
+	}
+}
