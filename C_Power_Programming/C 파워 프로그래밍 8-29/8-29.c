085e9017502929d9bbbb78e6ba65330129220ba8
piecemakerz <piecemakerz@naver.com>
2017-05-17 10:43:12 +0900

    8-29 : 다시 풀어봄, C언어를 배우자 카페 5월 16일자 질문 참고

--- C_Power_Programming/C 파워 프로그래밍 8-29/8-29.c
+++ C_Power_Programming/C 파워 프로그래밍 8-29/8-29.c
@@ -70,3 +70,67 @@ int InsertList(bookInfo bookPtr) {
 
 	return ++numOfData;
 }
+
+/* 내가 푼 풀이
+#include <stdio.h>
+#include <string.h>
+#define BOOK_INFO_NUM 3
+#define STR_LEN 50
+typedef struct __bookInfo {
+	char bookTitle[STR_LEN];
+	char bookPub[STR_LEN];
+	int bookPrice;
+} bookInfo;
+
+bookInfo books[BOOK_INFO_NUM] = { 0, };
+void InsertBooks(bookInfo input);
+
+int main(void) {
+	bookInfo save;
+
+	puts("******도서 정보 입력******");
+	for (int i = 0; i < 3; i++) {
+		printf("%d번째 도서 정보 입력.\n", i + 1);
+		fputs("도서 제목 : ", stdout);
+		gets(save.bookTitle);
+		fputs("출판사 명 : ", stdout);
+		gets(save.bookPub);
+		fputs("도서 가격 : ", stdout);
+		scanf("%d", &(save.bookPrice));
+		getchar();
+		InsertBooks(save);
+		printf("%d번째 입력 완료......\n", i + 1);
+		puts("");
+	}
+	puts("******도서 정보 출력******");
+	for (int i = 0; i < 3; i++) {
+		printf("%d번째 도서 정보 출력.\n", i + 1);
+		fputs("도서 제목 : ", stdout);
+		puts(books[i].bookTitle);
+		fputs("출판사 명 : ", stdout);
+		puts(books[i].bookPub);
+		fputs("도서 가격 : ", stdout);
+		printf("%d\n", books[i].bookPrice);
+		puts("");
+	}
+}
+
+void InsertBooks(bookInfo input) {
+	int num = 0;
+	while (1) {
+		if (books[num].bookTitle[0] == 0) {
+			books[num] = input;
+			break;
+		}
+		if (strcmp(books[num].bookTitle, input.bookTitle) > 0) {
+			for (int i = BOOK_INFO_NUM - 2; i >= num; i--) {
+				books[i + 1] = books[i];
+			}
+			books[num] = input;
+			break;
+		}
+		num++;
+	}
+	return;
+}
+*/
