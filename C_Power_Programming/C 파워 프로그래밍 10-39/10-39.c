c221a304d66beea88b9a122ada8a1800f34ded74
piecemakerz <piecemakerz@naver.com>
2017-05-18 12:24:31 +0900

    10-39 : 다시 풀어봄

--- C_Power_Programming/C 파워 프로그래밍 10-39/10-39.c
+++ C_Power_Programming/C 파워 프로그래밍 10-39/10-39.c
@@ -113,3 +113,93 @@ int main(void) {
 	return 0;
 	// 여기까지 문제 39-3
 }
+
+/*내 코드
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_STR_LEN 100
+
+int main(void) {
+	int date;
+	char area[MAX_STR_LEN];
+	char ap;
+	char weather[MAX_STR_LEN];
+
+	FILE * fp = fopen("weatherinfo.txt", "wt");
+	while (1) {
+		puts("******날씨 정보 입력******");
+		fputs("날짜 : ", stdout);
+		if (scanf("%d", &date) == EOF)
+			break;
+		getchar();
+		fwrite((void*)&date, sizeof(int), 1, fp);
+
+		fputs("지역 : ", stdout);
+		fgets(area, MAX_STR_LEN, stdin);
+		fputs(area, fp);
+
+		fputs("오전(A)/오후(P) : ", stdout);
+		ap = fgetc(stdin);
+		fputc(ap, fp);
+		getchar();
+
+		fputs("날씨 : ", stdout);
+		fgets(weather, MAX_STR_LEN, stdin);
+		fputs(weather, fp);
+	}
+	puts("입력이 완료되었습니다.");
+	puts("");
+	fclose(fp);
+
+	fp = fopen("weatherinfo.txt", "rt");
+	while (1) {
+		fread((void*)&date, sizeof(int), 1, fp);
+		if (feof(fp) != 0)
+			break;
+
+		fgets(area, MAX_STR_LEN, fp);
+		ap = fgetc(fp);
+		fgets(weather, MAX_STR_LEN, fp);
+		printf("날짜 : %d\n", date);
+		printf("지역 : %s", area);
+		printf("오전(A)/오후(P) : %c\n", ap);
+		printf("날씨 : %s", weather);
+		puts("");
+	}
+
+	puts("출력이 완료되었습니다.");
+	fclose(fp);
+
+	fp = fopen("weatherinfo.txt", "rt");
+	int filedate;
+	char filearea[MAX_STR_LEN];
+	char fileap;
+	char fileweather[MAX_STR_LEN];
+
+	fputs("검색할 날짜 및 지역 정보 입력 : ", stdout);
+	scanf("%d %s %c", &date, area, &ap);
+	area[strlen(area)] = '\n';
+	area[strlen(area) + 1] = 0;
+	while (1) {
+		fread((void*)&filedate, sizeof(int), 1, fp);
+		if(feof(fp)!=0){
+			puts("해당 날짜 및 지역의 데이터가 존재하지 않습니다.");
+			break;
+		}
+		fgets(filearea, MAX_STR_LEN, fp);
+		fileap = fgetc(fp);
+		fgets(fileweather, MAX_STR_LEN, fp);
+		if ((filedate == date) && (strcmp(filearea, area) == 0) && ap == fileap) {
+			puts(fileweather);
+			break;
+		}
+
+	}
+	fclose(fp);
+	return 0;
+}
+
+
+*/
