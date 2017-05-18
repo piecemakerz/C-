db6c56f490af4cbe6665fe97c45f56a77fdac2c1
piecemakerz <piecemakerz@naver.com>
2017-05-18 12:20:03 +0900

    10-40 : 다시 풀어봄

--- C_Power_Programming/C 파워 프로그래밍 10-40/10-40.c
+++ C_Power_Programming/C 파워 프로그래밍 10-40/10-40.c
@@ -92,3 +92,98 @@ int main(void) {
 	fclose(fp2);
 	return 0;
 }
+
+/* 내 코드
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_STR_LEN 100
+
+int main(void) {
+	FILE * fp = fopen("string.txt", "wt");
+	char str1[MAX_STR_LEN];
+	char str2[MAX_STR_LEN];
+	char ch;
+	puts("------ 문자열 입력 및 저장------");
+	fputs("첫 번째 문자열 : ", stdout);
+	fgets(str1, MAX_STR_LEN, stdin);
+	fwrite((void*)str1, sizeof(char), strlen(str1), fp);
+	fputs("두 번째 문자열 : ", stdout);
+	fgets(str2, MAX_STR_LEN, stdin);
+	fwrite((void*)str2, sizeof(char), strlen(str2), fp);
+	puts("");
+	fclose(fp);
+
+	fp = fopen("string.txt", "rt");
+	int count = 0;
+	puts("------ 저장된 문자열 출력------");
+	fputs("첫 번째 문자열 : ", stdout);
+	while (1) {
+		fread((void*)&ch, sizeof(char), 1, fp);
+		if (ch == '\n') {
+			str1[count] = ch;
+			str1[count+1] = 0;
+			break;
+		}
+		str1[count++] = ch;
+	}
+	fputs(str1, stdout);
+	count = 0;
+
+	fputs("두 번째 문자열 : ", stdout);
+	while (1) {
+		fread((void*)&ch, sizeof(char), 1, fp);
+		if (ch == '\n') {
+			str2[count] = ch;
+			str2[count+1] = 0;
+			break;
+		}
+		str1[count++] = ch;
+	}
+	fputs(str2, stdout);
+	puts("\n");
+	fclose(fp);
+
+	char filename[MAX_STR_LEN];
+	char copyname[MAX_STR_LEN];
+	fputs("원본 이름 : ", stdout);
+	gets(filename);
+	fputs("복사본 이름 : ", stdout);
+	gets(copyname);
+	
+	fp = fopen(filename, "rb");
+	FILE * copyfp = fopen(copyname, "wb");
+	while (1) {
+		ch = fgetc(fp);
+		if (feof(fp))
+			break;
+		fputc(ch, copyfp);
+	}
+	fclose(fp);
+	fclose(copyfp);
+	puts("복사가 완료되었습니다.");
+
+	fputs("원본 이름 : ", stdout);
+	gets(filename);
+	fputs("복사본 이름 : ", stdout);
+	gets(copyname);
+
+	fp = fopen(filename, "rb");
+	copyfp = fopen(copyname, "wb");
+	while (1) {
+		count = fread((void*)str1, sizeof(char), MAX_STR_LEN, fp);
+		if (count < MAX_STR_LEN) {
+			if (feof(fp)==0) {
+				puts("파일을 읽는 중 오류가 발생했습니다.");
+				break;
+			}
+			fwrite((void*)str1, sizeof(char), MAX_STR_LEN, copyfp);
+			puts("복사가 완료되었습니다.");
+			break;
+		}
+		fwrite((void*)str1, sizeof(char), MAX_STR_LEN, copyfp);
+	}
+	fclose(fp);
+	fclose(copyfp);
+	return 0;
+*/
