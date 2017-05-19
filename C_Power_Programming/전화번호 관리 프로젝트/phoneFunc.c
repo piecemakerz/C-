2b7694ac383af355a910c1f65f5224870bd6a0d5
piecemakerz <piecemakerz@naver.com>
2017-05-19 11:36:57 +0900

    전화번호 관리 프로젝트 4단계 - 동명이인 처리하기

--- C_Power_Programming/전화번호 관리 프로젝트/phoneFunc.c
+++ C_Power_Programming/전화번호 관리 프로젝트/phoneFunc.c
@@ -1,5 +1,5 @@
 /*
- * Name : phoneFunc.c ver 1.3
+ * Name : phoneFunc.c ver 1.4
  * Content : 전화번호 컨트롤 함수
  * Implementation : piecemakerz
  *
@@ -26,18 +26,32 @@ phoneData * phoneList[LIST_NUM];
 void InputPhoneData(void)
 {
 	// phoneData data;
-	phoneData * data = (phoneData*)malloc(sizeof(phoneData));
+	phoneData * data;
+
 	if (numOfData >= LIST_NUM)
 	{
 		puts("메모리 공간이 부족합니다.");
 		return;
 	}
+
+	data = (phoneData*)malloc(sizeof(phoneData));
+
 	fputs("이름을 입력하세요 : ", stdout);
 	//gets(data.name);
 	gets(data->name);
 	fputs("전화번호를 입력하세요 : ", stdout);
 	//gets(data.phoneNum);
 	gets(data->phoneNum);
+
+	for (int i = 0; i < numOfData; i++) {
+		if (!strcmp(phoneList[i]->name, data->name) && 
+			!strcmp(phoneList[i]->phoneNum, data->phoneNum)) {
+			free(data);
+			puts("이미 입력된 데이터 입니다.");
+			getchar();
+			return;
+		}
+	}
 
 	phoneList[numOfData++] = data;
 
@@ -65,7 +79,7 @@ void ShowAllData(void) {
 
 void SearchPhoneData(void) {
 	char name[NAME_LEN];
-
+	int namefound = 0;
 	fputs("찾는 이름은? ", stdout);
 	gets(name);
 
@@ -74,37 +88,71 @@ void SearchPhoneData(void) {
 		if (!strcmp(phoneList[i]->name, name)) {
 			//ShowPhoneInfo(phoneList[i]);
 			ShowPhoneInfoByPtr(phoneList[i]);
-			puts("검색이 완료되었습니다.");
-			getchar();
-			return;
+
+			//getchar();
+			//return;
+
+			namefound++;
 		}
 	}
 	
-	puts("찾는 이름의 데이터가 존재하지 않습니다.");
+	if (namefound)
+		puts("검색이 완료되었습니다.");
+	else
+		puts("찾는 이름의 데이터가 존재하지 않습니다.");
 	getchar();
+
 	return;
 }
 
 void DeletePhoneData(void) {
 	char name[NAME_LEN];
-
+	int samenameidx[LIST_NUM];
+	int samecount = 0;
+	int choice;
 	fputs("찾는 이름은? ", stdout);
 	gets(name);
 
 	for (int i = 0; i < numOfData; i++) {
 		// if (!strcmp(phoneList[i].name, name))
 		if (!strcmp(phoneList[i]->name, name)) {
-			free(phoneList[i]);
+			samenameidx[samecount++] = i;
+			/*free(phoneList[i]);
 			for (int j = numOfData-1; j > i; j--)
 				phoneList[j - 1] = phoneList[j];
 			numOfData--;
 			puts("삭제가 완료되었습니다.");
 			getchar();
 			return;
+			*/
 		}
 	}
+	if (!samecount) {
+		puts("찾는 이름의 데이터가 존재하지 않습니다.");
+		getchar();
+		return;
+	}
+	else if (samecount == 1) {
+		choice = 1;
+	}
+	else {
+		for (int i = 0; i < samecount; i++) {
+			printf("NUM. %d\n", i + 1);
+			ShowPhoneInfoByPtr(phoneList[samenameidx[i]]);
+			puts("");
+		}
 
-	puts("찾는 이름의 데이터가 존재하지 않습니다.");
+		fputs("선턕 : ", stdout);
+		scanf("%d", &choice);
+		getchar();
+	}
+	free(phoneList[samenameidx[choice-1]]);
+
+	for (int i = samenameidx[choice-1]; i<numOfData-1 ;i++)
+		phoneList[i] = phoneList[i+1];
+
+	numOfData--;
+	puts("삭제가 완료되었습니다.");
 	getchar();
 	return;
 }
